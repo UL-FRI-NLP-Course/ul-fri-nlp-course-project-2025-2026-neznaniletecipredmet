6012ba391a0aa0d7462c6b01d150786a8f30651f
Iztok Bajcar <iztok.bajcar@gmail.com>
2026-04-27 20:51:19 +0200

    Fixed torch version and loaded CUDA module

--- code/slurm/build_index.sh
+++ code/slurm/build_index.sh
@@ -9,16 +9,22 @@
 #SBATCH --output=logs/build_index_%j.out
 #SBATCH --error=logs/build_index_%j.err
 
+cd "$SLURM_SUBMIT_DIR/.."
 export HF_HOME=FILL_IN_SHARED_CACHE_DIR
+
+
+module load CUDA/12.2.0
 module load Python/3.11
 
 if [ ! -d ".venv" ]; then
     python -m venv .venv
 fi
 
+ls -al .venv/bin
 source .venv/bin/activate
-pip install -r ../requirements.txt
 
-cd "$SLURM_SUBMIT_DIR/.."
+# install torch from the official website to avoid errors
+pip install -r requirements.txt
+pip install torch==2.1.2 --index-url https://download.pytorch.org/whl/cu118
 
 python scripts/build_index.py
