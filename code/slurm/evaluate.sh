8a423e0141923b737eba97c212fc636dc81269ca
Iztok Bajcar <iztok.bajcar@gmail.com>
2026-04-29 08:05:49 +0200

    Fixed cache path in other slurm scripts

--- code/slurm/evaluate.sh
+++ code/slurm/evaluate.sh
@@ -9,7 +9,7 @@
 #SBATCH --output=logs/evaluate_%j.out
 #SBATCH --error=logs/evaluate_%j.err
 
-export HF_HOME=FILL_IN_SHARED_CACHE_DIR
+export HF_HOME=/d/hpc/projects/onj_fri/neznani-leteci-predmet/cache
 
 source FILL_IN_VENV_ACTIVATE_PATH
 
